10b275d3b43fd05ef4f90ece969cf73173e34a10
Gunnar <Gunnar@lmdv.se>
2020-08-31 21:48:06 +0200

    pyton-sql-html

---
+++ Docker/Java/src/Container.java
@@ -0,0 +1,25 @@
+
+public class Container extends Ship {
+
+	public Container() {
+
+	}
+
+	public Container(int shipId, int shipLogId, int maxSpeed, int cruisingSpeed, String name, int currentSpeed,
+			String bearing, String cargo, String currentCoordinates, String startCoordinates,
+			String destinationCoordinates, String route, int nauticMilage, boolean docked, int maxCargoWeight,
+			int cargoWeight, int[] previousCoordinates) {
+		super(shipId, shipLogId, maxSpeed, cruisingSpeed, name, currentSpeed, bearing, cargo, currentCoordinates,
+				startCoordinates, destinationCoordinates, route, nauticMilage, docked, maxCargoWeight, cargoWeight,
+				previousCoordinates);
+	}
+
+	@Override
+	public void unloadAndLoad() {
+		// System.out.println("Unloading containers...");
+		this.setCargoWeight(0);
+		// System.out.println("Loading containers...");
+		this.setCargoWeight(this.getMaxCargoWeight());
+	}
+
+}
