53b47d702264be097c973a8299857b716428440c
Fashgubben <timojof@gmail.com>
2020-09-02 07:11:49 +0000

    Merge onse.py solved

--- Docker/Python/databaspythonv3.py
+++ Docker/Python/databaspythonv3.py
@@ -37,12 +37,8 @@ while 1:
         sql = "select * from uvshipposition"
         cur = db.query(sql)
         result = cur.fetchall() 
-<<<<<<< HEAD
         
-        with open('/html/test.html', 'w') as f:
-=======
         with open('/code/html/test.html', 'w') as f:
->>>>>>> c2ee5e9c4a4c2db0a592e8bcc50495b7beca42d0
             f.write(""" <!DOCTYPE html>
                         <html>
                         <head>
