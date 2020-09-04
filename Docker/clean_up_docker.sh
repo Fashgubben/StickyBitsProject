4b408b7805cb10125d1adb9ff0707590a452cf2f
Fashgubben <timojof@gmail.com>
2020-09-04 10:15:27 +0000

    test-commit

--- Docker/clean_up_docker.sh
+++ Docker/clean_up_docker.sh
@@ -2,4 +2,10 @@
 
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
-docker rmi $(docker images -a -q)
+docker rmi docker_app-python
+docker rmi docker_app-java
+docker rmi python
+docker rmi openjdk
+docker rmi httpd
+docker rmi mysql
+
