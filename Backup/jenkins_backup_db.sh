a20bd4ccac19b58952adfc00503252fcd4eef544
timojof@gmail.com <timojof@gmail.com>
2020-09-07 14:05:00 +0200

    Merge again

--- Backup/jenkins_backup_db.sh
+++ Backup/jenkins_backup_db.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 pwd
 
-backup_dir=./Docker/Mysql/mysql-dump
+backup_dir=../Docker/Mysql/mysql-dump
 db_name="ArmadaDataBase"
 
 # Stops java container. 
 docker stop docker_app-java_1
 
 # Removes old sql-file
-rm ./Docker/Mysql/mysql-dump/ArmadaDataBase.sql
+rm ../Docker/Mysql/mysql-dump/ArmadaDataBase.sql
 
 # Takes backup and place in right directory 
 docker exec docker_mysql-db_1 /usr/bin/mysqldump -u root --password=password fleet > $backup_dir/$db_name.sql
